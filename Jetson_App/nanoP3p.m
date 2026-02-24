d547fbbb184a69c134c2118cd0534b1646f3b53a
LithembaBaninzi <160032740+LithembaBaninzi@users.noreply.github.com>
2026-02-24 11:31:29 +0200

    Latest files

--- Jetson_App/nanoP3p.m
+++ Jetson_App/nanoP3p.m
@@ -1,4 +1,4 @@
-function [bestPose] = nanoP3p(img)
+function [bestPose_rc2rw] = nanoP3p(img)
 %NANOP3P Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -9,15 +9,15 @@ K = [  583.6734         0  309.7243;   0  582.8750  183.5180;   0         0    1
 
 %%
 [x_det, X_W_det, id_det] = featureDetectMatch_nano(rgb2gray(img), featMap.featureMap); %detect Aruco tags
-[x_train, XW_train, x_test, XW_test] = featSelect(x_det, X_W_det, id_det); %select features
+[x_train, XW_train, id_train, x_test, XW_test, id_test] = featSelect(x_det, X_W_det, id_det', 1); %select features
 
-bestPose = nan(7,1);
+bestPose_rc2rw = nan(7,1);
 if ~isnan(x_det(1,1))
     Rt_arr = kneipWrapper(x_train, XW_train, K); %run pose estimator - gives up to four solutions, returns NaN if it can't see any features
     %Rt_best = chooseSoln(Rt_arr, x_test, XW_test, K);
     if ~isnan(Rt_arr(1,1,1))   
         [Rt_best, numIn,~] = chooseRtWithMostInliers(K, Rt_arr, 1, x_test, XW_test);  
-        bestPose = rtToPose(Rt_best);
+        bestPose_rc2rw = rtToPose(Rt_best);
     end
 else
 
